4a6212002ae3124df5f827bf32d17f12df3408c1
Y1903 <jnazarin0319@gmail.com>
2024-09-02 11:37:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+// task 1
+let a = 2;
+let b = 5;
+let x = a * b;
+console.log(x);
+
+// task 2
+let c = 9;
+let d = 3;
+let y = c / d;
+console.log(y);
+
+// task 3
+let e = 4;
+let f = 8;
+let z = e + f;
+console.log(z);
+
+// tassk 4
+let h = 11;
+let j = "java script ";
+let k = true;
+let l = "100";
+console.log(h, j, k, l);
+
+// task 5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+console.log(num);
+
+// task 6
+let g = 9;
+g = g * g;
+console.log(g);
+
+// task 7
+let q = prompt("введіть число:");
+let squaredNumber = q * q;
+alert("Результат:" + squaredNumber);
+
+// task 8
+let r = +prompt("введіть 1 число:");
+let t = +prompt("введіть 2 число:");
+let average = (r + t) / 2;
+alert("Результат:" + average);
+
+// task 9
+let v = prompt("введіть число хвилин:");
+let numberOfSeconds = v * 60;
+alert("Результат:" + numberOfSeconds);
+
+// task 10
+let greeting = "Hello";
+let userName = prompt("введіть свое імʼя");
+alert("Hello," + userName + "!");
